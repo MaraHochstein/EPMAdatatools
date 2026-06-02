127b9d8b0bd54b2c3c66e49e7857435c1d71f20f
ml <mara.hochstein@gmx.de>
2026-06-02 14:34:24 +0200

    Update mineral-identification.py

--- pages/mineral-identification.py
+++ pages/mineral-identification.py
@@ -773,7 +773,7 @@ else:
             st.markdown('The probability of predicted minerals is calculated with respect to the site occupation and the amount of available elements (measured values). These values are calculated:')
             st.markdown(
             r"""
-            $\frac{a}{b}=1$ 
+            - $\frac{a}{b}=1$: to what percentage could the sites be filled?
             <ul>
                 <li>
                     <img src="./app/static/percentFilled.png" height="20" style="margin-right: 5px; margin-left: 5px">: to what percentage could the sites be filled?
